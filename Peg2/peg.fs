d52c98663ef742f0906c542f9e2995a7b227abff
Paul C Roberts <pacaro@gmail.com>
2013-01-20 15:25:16 -0800

    Second PEG blog article

---
+++ Peg2/peg.fs
@@ -0,0 +1,116 @@
+﻿module Peg
+
+type Expression =
+| Terminal of string
+| NonTerminal of string
+| Epsilon
+| Sequence of Expression list
+| Choice of Expression list
+| ZeroOrMore of Expression
+| OneOrMore of Expression
+| Optional of Expression
+| And of Expression
+| Not of Expression
+
+let rec pegToString expr =
+    let pegToStringParen = function
+    | Terminal _ | NonTerminal _ | Epsilon as x -> pegToString x
+    | x -> "(" + pegToString x + ")"
+
+    match expr with
+    | Terminal x -> "\"" + x + "\""
+    | NonTerminal x -> x
+    | Epsilon -> "<epsilon>"
+    | Sequence x -> List.map pegToStringParen >> List.reduce (fun a b -> a + " " + b) <| x
+    | Choice x -> List.map pegToStringParen >> List.reduce (fun a b -> a + " / " + b) <| x
+    | ZeroOrMore x -> pegToStringParen x + "*"
+    | OneOrMore x -> pegToStringParen x + "+"
+    | Optional x -> pegToStringParen x + "?"
+    | And x -> "&" + pegToStringParen x
+    | Not x -> "!" + pegToStringParen x
+
+
+let printPeg expr =
+    printfn "%s" <| pegToString expr
+
+type ParseResult<'a> =
+| Parsed of 'a
+| TerminalSymbol of string
+| Production of ParseResult<'a> list
+| EmptyMatch
+| Unmatched
+
+type GrammarRule<'a>(prod:Expression, func:(string -> ParseResult<'a> -> ParseResult<'a>)) =
+    member this.Prod = prod
+    member this.Func = func
+
+let parseExpression (grammar:Map<string,GrammarRule<'a>>) start (input:string) =
+    let rec parse offset = function
+    | Terminal x -> 
+        let e = offset + x.Length - 1
+        if e < input.Length && input.[offset..e] = x then (TerminalSymbol x, e + 1) else (Unmatched, offset)
+    | NonTerminal x -> 
+        let rule = grammar.[x]
+        match parse offset rule.Prod with 
+        | (Unmatched, _) as y -> y
+        | (parsed, endOffset) -> (rule.Func input.[offset..endOffset - 1] parsed, endOffset)
+    | Epsilon -> if offset = input.Length then (EmptyMatch, offset) else (Unmatched, offset)
+    | Sequence x ->
+        let rec parseList lst off =
+            seq {
+                if List.isEmpty lst then ()
+                else
+                    match parse off <| List.head lst with
+                    | (Unmatched, _) as y -> yield y
+                    | (y, z) -> yield (y, z)
+                                yield! parseList (List.tail lst) z
+            }
+        
+        let s = List.ofSeq <| parseList x offset 
+        if List.exists (function | (Unmatched, _) -> true | _ -> false) s then (Unmatched, offset)
+        else (Production <| List.map fst s, snd <| List.maxBy snd s)
+    | Choice x ->
+        let rec parseList lst =
+            if List.isEmpty lst then (Unmatched, offset)
+            else
+                match parse offset <| List.head lst with
+                | (Unmatched, _) -> parseList <| List.tail lst
+                | y -> y
+        
+        parseList x
+    | ZeroOrMore x ->
+        let rec parseList off =
+            seq {
+                match parse off x with
+                | (Unmatched, _) -> ()
+                | (y, z) -> yield (y, z)
+                            yield! parseList z
+            }
+        let s = List.ofSeq <| parseList offset
+        if List.isEmpty s then (EmptyMatch, offset) 
+        else (Production <| List.map fst s, snd <| List.maxBy snd s)
+    | OneOrMore x ->
+        let rec parseList off =
+            seq {
+                match parse off x with
+                | (Unmatched, _) -> ()
+                | (y, z) -> yield (y, z)
+                            yield! parseList z
+            }
+        let s = List.ofSeq <| parseList offset
+        if List.isEmpty s then (Unmatched, offset)
+        else (Production <| List.map fst s, snd <| List.maxBy snd s)
+    | Optional x ->
+        match parse offset x with
+        | (Unmatched, _) -> (EmptyMatch, offset)
+        | y -> y
+    | And x ->
+        match parse offset x with
+        | (Unmatched, _) -> (Unmatched, offset)
+        | _ -> (EmptyMatch, offset)
+    | Not x ->
+        match parse offset x with
+        | (Unmatched, _) -> (EmptyMatch, offset)
+        | _ -> (Unmatched, offset)
+
+    parse 0 <| NonTerminal start
---
+++ Peg2/test.fs
@@ -0,0 +1,106 @@
+﻿open Peg
+
+let unexpected x =
+    printfn "Unexpected pattern %A" x
+    failwith "parse error"
+
+let g = 
+    // bool <- "true" / "false"
+    let boolRule = GrammarRule<bool>(Choice [Terminal "true"; Terminal "false"], (fun s _ -> Parsed (s = "true")))
+
+    // paren <- "(" expr ")"
+    let parseParen _ = function
+    | Production [TerminalSymbol "("; x; TerminalSymbol ")"] -> x
+    | x -> unexpected x
+    let parenRule = GrammarRule<bool>(Sequence [Terminal "("; NonTerminal "expr"; Terminal ")"], parseParen)
+
+    // not <- "!" atom
+    let parseNot _ = function
+    | Production [TerminalSymbol "!"; Parsed x] -> Parsed <| not x
+    | x -> unexpected x
+    let notRule = GrammarRule<bool>(Sequence [Terminal "!"; NonTerminal "atom"], parseNot)
+
+    // atom <- bool / paren / not
+    let atomRule = GrammarRule<bool>(Choice [NonTerminal "bool"; NonTerminal "paren"; NonTerminal "not"], (fun _ x -> x))
+
+    // and <- atom ("&" and)?
+    // or <- and ("!" or)?
+    let parseBin _ = function
+    | Production [x; EmptyMatch] -> x
+    | Production [Parsed x; Production [TerminalSymbol "&"; Parsed y]] -> Parsed (x && y)
+    | Production [Parsed x; Production [TerminalSymbol "|"; Parsed y]] -> Parsed (x || y)
+    | x -> unexpected x
+    let andRule = GrammarRule<bool>(Sequence [NonTerminal "atom"; Optional (Sequence [Terminal "&"; NonTerminal "and"])], parseBin)
+    let orRule = GrammarRule<bool>(Sequence [NonTerminal "and"; Optional (Sequence [Terminal "|"; NonTerminal "or"])], parseBin)
+
+    // expr <- or
+    let exprRule = GrammarRule<bool>(NonTerminal "or", (fun _ x -> x))
+
+    // start <- expr <epsilon>
+    let parseStart _ = function
+    | Production [x; EmptyMatch] -> x
+    | x -> unexpected x
+    let startRule = GrammarRule<bool>(Sequence [NonTerminal "expr"; Epsilon], parseStart)
+
+    Map.ofList [("bool", boolRule); ("paren", parenRule); ("not", notRule); ("atom", atomRule);
+                ("and", andRule); ("or", orRule); ("expr", exprRule); ("start", startRule)]
+
+type BooleanExpr =
+| Atom of bool
+| Binary of BooleanExpr * string * BooleanExpr
+| Not of BooleanExpr
+
+let rec evalBool = function
+| Atom x -> x
+| Binary (left, "&", right) -> if evalBool left then evalBool right else false
+| Binary (left, "|", right) -> if evalBool left then true else evalBool right
+| Binary (_, x, _) -> failwith <| "Unexpected binary operator: " + x
+| Not x -> not <| evalBool x
+
+let boolGrammar = 
+    // bool <- "true" / "false"
+    let boolRule = GrammarRule<BooleanExpr>(Choice [Terminal "true"; Terminal "false"], (fun s _ -> Parsed (Atom (s = "true"))))
+    
+    // space <- " "*
+    let spaceRule = GrammarRule<BooleanExpr>(ZeroOrMore (Terminal " "), (fun _ _ -> EmptyMatch))
+
+    // paren <- "(" space expr space ")"
+    let boolParen _ = function
+    | Production [TerminalSymbol "("; EmptyMatch; x; EmptyMatch; TerminalSymbol ")"] -> x
+    | x -> unexpected x
+    let parenRule = GrammarRule<BooleanExpr>(Sequence [Terminal "("; NonTerminal "space"; NonTerminal "expr"; NonTerminal "space"; Terminal ")"], boolParen)
+
+    // not <- "!" atom
+    let boolNot _ = function
+    | Production [TerminalSymbol "!"; Parsed x] -> Parsed (Not x)
+    | x -> unexpected x
+    let notRule = GrammarRule<BooleanExpr>(Sequence [Terminal "!"; NonTerminal "atom"], boolNot)
+
+    // atom <- "bool" / "paren" / "not"
+    let atomRule = GrammarRule<BooleanExpr>(Choice [NonTerminal "bool"; NonTerminal "paren"; NonTerminal "not"], (fun _ x -> x))
+
+    // or <- atom space ("|" space or)?
+    // and <- or space ("&" space and)?
+    let boolBinary _ = function
+    | Production [x; EmptyMatch; EmptyMatch] -> x
+    | Production [Parsed x; EmptyMatch; Production [TerminalSymbol op; EmptyMatch; Parsed y]] -> Parsed (Binary (x, op, y))
+    | x -> unexpected x
+    let orRule = GrammarRule<BooleanExpr>(Sequence [NonTerminal "atom"; NonTerminal "space"; Optional (Sequence [Terminal "|"; NonTerminal "space"; NonTerminal "or"])], boolBinary)
+    let andRule = GrammarRule<BooleanExpr>(Sequence [NonTerminal "or"; NonTerminal "space"; Optional (Sequence [Terminal "&"; NonTerminal "space"; NonTerminal "and"])], boolBinary)
+
+    // expr <- and
+    let exprRule = GrammarRule<BooleanExpr>(NonTerminal "and", (fun _ x -> x))
+
+    // start <- space expr space <epsilon>
+    let boolStart _ = function
+    | Production [EmptyMatch; x; EmptyMatch; EmptyMatch] -> x
+    | x -> unexpected x
+    let startRule = GrammarRule<BooleanExpr>(Sequence [NonTerminal "space"; NonTerminal "expr"; NonTerminal "space"; Epsilon], boolStart)
+
+    Map.ofList [("bool", boolRule); ("space", spaceRule); ("paren", parenRule); ("not", notRule);
+                ("atom", atomRule); ("or", orRule); ("and", andRule); ("expr", exprRule); ("start", startRule)]
+
+let parseAndEvalBool expr =
+    match parseExpression boolGrammar "start" expr with
+    | (Parsed b, _) -> printfn "%s = %A" expr (evalBool b)
+    | x -> unexpected x
